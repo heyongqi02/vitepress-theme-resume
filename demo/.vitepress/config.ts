c21fc6229cb3fbb1606832116a7e30bc74da7800
bjmhe <hi@bjmhe.me>
2026-06-13 04:02:35 +0800

    feat: set base path for VitePress theme resume

--- demo/.vitepress/config.ts
+++ demo/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitepress";
 import { defineResumeConfig } from "vitepress-theme-resume/config";
 
 export default defineConfig({
+  base: "/vitepress-theme-resume/",
   extends: defineResumeConfig({
     title: "贺永琪",
     description: "前端开发工程师",
